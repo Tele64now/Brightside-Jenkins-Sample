45f16d8fa59c17b9bc6d0acaf2d17e0df3955927
MiltH <teleforever60@gmail.com>
2019-04-01 14:36:37 -0400

    Put "copy-dbrm" back in front on line 155

--- gulpfile.js
+++ gulpfile.js
@@ -152,4 +152,4 @@ gulp.task('build', 'Build Program', gulpSequence('build-cobol','build-lnk'));
   });
 }); 
 
- gulp.task('deploy', 'Deploy Program', gulpSequence('copy-load','copy-dbrm','bind-n-grant'));
+ gulp.task('deploy', 'Deploy Program', gulpSequence('copy-dbrm','copy-load','bind-n-grant'));
